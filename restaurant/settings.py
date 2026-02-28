d7ac9012a969e4b5e9a781fab80252da8c4cd787
Suresh-144 <yenugulasuresh144@gmail.com>
2026-02-28 19:20:23 +0530

    Update settings.py

--- restaurant/settings.py
+++ restaurant/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['projectlb-1866953302.ap-south-2.elb.amazonaws.com']
+ALLOWED_HOSTS = ['projectlb-1866953302.ap-south-2.elb.amazonaws.com','*']
 
 
 # Application definition
